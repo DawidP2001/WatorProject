4b81f9a20784c6fc85a4891ef1f0c55483f12087
DawidP2001 <c00273530@setu.ie>
2024-11-05 23:10:08 +0000

    first commit

---
+++ Simulation.go
@@ -0,0 +1,24 @@
+//--------------------------------------------
+// Author: Dawid Pionk
+// Created on 21/10/24
+// Description:
+// A solution to the dinining philosophers problem
+// Issues:
+// None I hope
+//--------------------------------------------
+
+package main
+
+func runSim() {
+	// Chronons pass
+}
+
+func main() {
+	numShark := 0   // Starting population of sharks
+	numFish := 0    // Starting population of fish
+	fishBreed := 0  // Number of time units that pass before a fish can reproduce
+	sharkBreed := 0 // Number of time units that must pass before a shark can reproduce;
+	starve := 0     // Period of time a shark can go without food before dying;
+	gridSize := 0   // Dimensions of world
+	threads := 0    // number of threads to use
+}
