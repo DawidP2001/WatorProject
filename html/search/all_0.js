faab96199394d0370104efb78241819c019e8a26
DawidP2001 <c00273530@setu.ie>
2024-11-25 13:10:22 +0000

    Added Makefile and made changes to the README

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['creature_2ego_0',['Creature.go',['../_creature_8go.html',1,'']]]
+  ['constants_2ego_0',['Constants.go',['../_constants_8go.html',1,'']]],
+  ['creature_2ego_1',['Creature.go',['../_creature_8go.html',1,'']]]
 ];
